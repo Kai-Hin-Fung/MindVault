1a3171ced2ba25a6315f781995fbbda1367e140b
Fung Kai Hin, James <37337207+Kai-Hin-Fung@users.noreply.github.com>
2023-06-12 18:24:35 +0100

    Mongo DB successful connected to nodejs
    Successful connection through mongoose, but attempt to post link into the database failed.

--- server/server.js
+++ server/server.js
@@ -1,41 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const Link = require('./models/Link');
+const Link = require('./models/link');
 
-
-// Set up Express
 const app = express();
-app.use(express.json()); // This allows us to access req.body
 const port = process.env.PORT || 5000;
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://MindVault2578:<2017DSEstriver>@cluster0.g0scskv.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('Database Connected Successfully'))
-.catch(err => console.log(err));
+// Use your MongoDB Atlas connection string here
+const dbURI = 'mongodb+srv://professionalhustler84:51rluT2yApGCGb0G@cluster1.dwbssp5.mongodb.net/?retryWrites=true&w=majority';
 
-// Set up CORS
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Database Connected Successfully'))
+    .catch(err => console.log(err));
+
 app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json()); // Parse JSON request bodies
 
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello from server!' });
+});
 
 app.post('/api/submit', async (req, res) => {
     try {
-      // Create a new instance of the Link model with the URL obtained from the request body
-      const newLink = new Link({
-        url: req.body.url
-      });
-  
-      // Save the newLink object to the database
-      await newLink.save();
-  
-      // Send a success response with a JSON object indicating that the link was saved successfully
-      res.status(200).json({ message: 'Link saved successfully' });
+        const newLink = new Link({
+            url: req.body.url
+        });
+        await newLink.save();
+        res.status(200).json({ message: 'Link saved successfully' });
     } catch (error) {
-      // If an error occurs during the try block, send an error response with the error message
-      res.status(500).json({ error: error.message });
+        res.status(500).json({ error: error.message });
     }
-  });
-  
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
